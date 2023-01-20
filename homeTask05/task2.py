50a6e4a07320dd2b6c8999b9ff1df24de5bf5c6f
serega2543 <serega2543@yandex.ru>
2023-01-20 18:34:31 +0600

    home task5

--- homeTask05/task2.py
+++ homeTask05/task2.py
@@ -1,13 +1,92 @@
-# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку,
+# чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+import random
+from random import randint, choice
+
+print(
+    '"Игра с конфетами"\n'
+    'В игре участвуют два игрока\n'
+    'Первый ход определяется жеребьевкой.\n'
+    'Игроки ходят, совершая ход друг после друга\n'
+    'Правила игры\n'
+    'У нас есть некоторое количество конфет\n'
+    'За один ход можно забрать не более определенного количества конфет, о котором мы договоримся\n'
+    'Тот, кому достанется последняя конфета - победил\n'
+)
+
+messages = ['Ваш ход брать конфеты', 'Возьмите конфеты',
+            'сколько конфет берем?', 'берите еще', 'Ваш ход']
+max_number_move = 0
 
 
-def delwords(my_text):
-    my_text = list(filter(lambda el: 'абв' not in el, my_text.split()))
-    return " ".join(my_text)
+def introduce_players():
+    player2 = input('Первый игрок, представьтесь\n')
+    player1 = 'Бот'
+    print(f'Очень приятно, сегодня Вы играете с искуственным интеллектом "{player1}"')
+    return [player1, player2]
 
 
-my_text = 'dfg абвыва ываабвыва, ыващшшхАБВ - ывад абв. F<Df,dffАБВабв аа'
+def sweets_game(players):
+    global max_number_move
+    total_sweets = int(input('Введите cколько всего у нас конфет:\n'))
+    max_number_move = int(input('Введите количество конфет, которое можно забрать за один ход:\n'))
+    first = random.randint(0, 1)
+    print(first)
+    print('Методом жеребьёвки определено, первый ходит', players[first])
+    return [total_sweets, max_number_move, first]
 
-new_text = delwords(my_text)
-print(my_text)
-print(new_text)
+
+max_move = max_number_move
+
+
+def game_player_vs_smart_bot(sweets, players, messages):
+    global max_number_move
+    count = sweets[2]
+
+    while sweets[0] > 0:
+        if sweets[0] == (max_number_move and sweets[0] < max_number_move and sweets[0] > 1):
+            move = sweets[0] - 1
+            print(f'Я забираю {move}')
+
+        elif not count % 2:
+            move = random.randint(1, sweets[1])
+            print(f'Я забираю {move}')
+        else:
+            print(f'{players[1]}, {choice(messages)}')
+            move = int(input())
+            if move > sweets[0] or move > sweets[1]:
+                print(
+                    f'Можно взять не более {sweets[1]} конфет, у нас всего {sweets[0]} конфет')
+                chance = 2
+                while chance > 0:
+                    if sweets[0] >= move <= sweets[1]:
+                        break
+                    print(f'Попробуйте ещё раз, у Вас {chance} попытки')
+                    move = int(input())
+                    chance -= 1
+                else:
+                    return print(f'Попыток не осталось. Game over!')
+        sweets[0] = sweets[0] - move
+        if sweets[0] > 0:
+            print(f'Осталось {sweets[0]} конфет')
+        else:
+            print('Все конфеты разобраны.')
+        count += 1
+    return players[not count % 2]
+
+
+players = introduce_players()
+sweets = sweets_game(players)
+
+winer = game_player_vs_smart_bot(sweets, players, messages)
+if not winer:
+    print('У нас нет победителя.')
+else:
+    print(
+        f'Поздравляю! В этот раз победил {winer}! Ему достаются все конфеты!')
